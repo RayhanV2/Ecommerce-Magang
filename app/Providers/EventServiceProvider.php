ae5007762c4f83ea0f045ef581bbbe50d3361845
VaValentine <Rayhanemr9@gmail.com>
2022-09-08 08:04:55 +0700

    second commit

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,21 +2,25 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
     /**
-     * The event to listener mappings for the application.
+     * The event listener mappings for the application.
      *
-     * @var array<class-string, array<int, class-string>>
+     * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        'cart.added' => [
+            'App\Listeners\CartUpdatedListener',
+        ],
+        'cart.updated' => [
+            'App\Listeners\CartUpdatedListener',
+        ],
+        'cart.removed' => [
+            'App\Listeners\CartUpdatedListener',
         ],
     ];
 
@@ -27,16 +31,8 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-    }
+        parent::boot();
 
-    /**
-     * Determine if events and listeners should be automatically discovered.
-     *
-     * @return bool
-     */
-    public function shouldDiscoverEvents()
-    {
-        return false;
+        //
     }
 }
